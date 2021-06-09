716cdc56ddcf9fd5e14eb1050cdb9945a679d97c
Jeet Patel <57315649+jeetpatel9@users.noreply.github.com>
2021-06-09 21:15:29 +0530

    made package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_doc():
 
 setup(
     name="django_restframework_2fa",
-    version="0.0.6",
+    version="0.0.8",
     description="Some description",
     long_description=get_doc(),
     long_description_content_type="text/markdown",
